da8b54297841bdd705f32b72d2f753d96700a1c6
okan kaipoglu <kaipoglu267@gmail.com>
2022-08-23 18:01:48 +0300

    fatura repository

---
+++ DataAccessLayer/Abstract/FaturaDal/IFaturaDal.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract.FaturaDal
+{
+    public interface IFaturaDal
+    {
+        Fatura Kaydet(Fatura entity);
+        List<Fatura> Listele();
+        List<Fatura> Listele(Expression<Func<Fatura, bool>> filter);
+        Fatura Getir(int id);
+        int Guncelle(Fatura entity);
+        bool Sil(int id);
+        bool Sil(Fatura entity);
+        IQueryable FaturaRaporu(DateTime baslangic, DateTime bitis);
+
+    }
+}
---
+++ DataAccessLayer/Concrete/EntityFramework/Repository/EfFaturaRepository.cs
@@ -0,0 +1,54 @@
+﻿using DataAccessLayer.Abstract.FaturaDal;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.EntityFramework.Repository
+{
+    public class EfFaturaRepository : IFaturaDal
+    {
+        public IQueryable FaturaRaporu(DateTime baslangic, DateTime bitis)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Fatura Getir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Guncelle(Fatura entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Fatura Kaydet(Fatura entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Fatura> Listele()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Fatura> Listele(Expression<Func<Fatura, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Sil(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Sil(Fatura entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
